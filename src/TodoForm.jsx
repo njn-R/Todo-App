0680f6b8f854d41ddc5649fa6e0906a2e494ef76
njn-R <njn@protonmail.com>
2023-04-19 19:02:03 +0530

    Simple React Todo App

---
+++ src/TodoForm.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+export function TodoForm({ onSubmit }){
+  const [newItem, setnewItem] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if(newItem === "") return 
+    onSubmit(newItem)
+    setnewItem("")
+  }
+    
+  return (
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input value={newItem} onChange={e => setnewItem(e.target.value)} type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+  )
+}
